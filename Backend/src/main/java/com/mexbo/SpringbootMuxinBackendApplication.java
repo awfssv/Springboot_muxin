6586e7c166b4b32bad2aa026baf586cbc48fa05c
mexbochen <mexbochen@foxmail.com>
2018-11-22 23:13:21 +0800

    初始化后端代码

---
+++ Backend/src/main/java/com/mexbo/SpringbootMuxinBackendApplication.java
@@ -0,0 +1,12 @@
+package com.mexbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMuxinBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMuxinBackendApplication.class, args);
+    }
+}
