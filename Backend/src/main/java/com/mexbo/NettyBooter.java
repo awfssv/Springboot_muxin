6dfea82c6b273bf177cddaea8f9603c692f1b160
mexbochen <mexbochen@foxmail.com>
2018-11-23 01:09:39 +0800

    springboot_muxin整合netty

---
+++ Backend/src/main/java/com/mexbo/NettyBooter.java
@@ -0,0 +1,22 @@
+package com.mexbo;
+
+import com.mexbo.netty.WSServer;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NettyBooter implements ApplicationListener<ContextRefreshedEvent> {
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		if (event.getApplicationContext().getParent() == null) {
+			try {
+				WSServer.getInstance().start();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
