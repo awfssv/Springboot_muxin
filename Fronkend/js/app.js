8486ea3e958d1042ece30a3f9d8f2ec1ac04195e
mexbochen <mexbochen@foxmail.com>
2018-11-23 01:31:55 +0800

    实现登陆界面
    实现登陆界面，还没有连接后端数据库

---
+++ Fronkend/js/app.js
@@ -0,0 +1,16 @@
+window.app = {
+	/**
+	 * netty服务后端发布的url地址
+	 */
+	nettyServerUrl: 'ws://192.168.1.3:8088/ws',
+	
+	/**
+	 * 后端服务发布的url地址
+	 */
+	serverUrl: 'http://192.168.1.3:8080',
+	
+	/**
+	 * 图片服务器的url地址
+	 */
+	imgServerUrl: 'http://192.168.1.70:88/imooc/',
+}
