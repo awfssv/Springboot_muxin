e193a8efa0139bb4367f4aa0222b9f2a3084fd7c
mexbochen <mexbochen@foxmail.com>
2018-11-23 16:38:52 +0800

    整合FastDFS

---
+++ Backend/src/main/java/com/mexbo/FastdfsImporter.java
@@ -0,0 +1,22 @@
+package com.mexbo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
+
+import com.github.tobato.fastdfs.FdfsClientConfig;
+
+/**
+ * 导入FastDFS-Client组件
+ * 
+ * @author tobato
+ *
+ */
+@Configuration
+@Import(FdfsClientConfig.class)
+// 解决jmx重复注册bean的问题
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+public class FastdfsImporter {
+    // 导入依赖组件
+}
